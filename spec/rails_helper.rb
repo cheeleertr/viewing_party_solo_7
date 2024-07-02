2ab1ccb541974a864d7425ce314829ba06c78609
Chee Lee <cheeleertr@gmail.com>
2024-07-02 18:50:13 -0500

    add api key, gems, update helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,3 +71,13 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+#need to be modified
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  #CHANGE API KEY
+  config.filter_sensitive_data('DONT_SHARE_MY_SECRET_KEY') { Rails.application.credentials.tmdb[:key] }
+  config.default_cassette_options = { re_record_interval: 7.days }
+  config.configure_rspec_metadata!
+end
